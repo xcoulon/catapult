831759f87f82dc9308274cca91545467ca71384f
Scott Stark <starksm64@gmail.com>
2016-04-08 19:02:31 -0700

    Issue #30 Refactor Test JUL Config so it may be reused

--- core/core-api/src/test/java/org/rhd/katapult/test/LoggingConfig.java
+++ core/core-api/src/test/java/org/rhd/katapult/test/LoggingConfig.java
@@ -1,4 +1,4 @@
-package org.rhd.katapult.test.services;
+package org.rhd.katapult.test;
 
 import java.io.InputStream;
 import java.net.URL;
@@ -10,8 +10,8 @@ import java.util.logging.SimpleFormatter;
 
 /**
  * A logging configuration class that first attempts to load a logging.properties file from the classpath, and then falls
- * back to a programmatic configuration. To enable, specify -Djava.util.logging.config.class=LoggingConfig in test
- * app configuration.
+ * back to a programmatic configuration. To enable, specify -Djava.util.logging.config.class=org.rhd.katapult.test.LoggingConfig
+ * in test app configuration.
  *
  */
 public class LoggingConfig {
